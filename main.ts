d749bdb5d6a6cd87491b189d93f1da710e2d5448
Klaas Bosteels <klaas.bosteels@gmail.com>
2025-12-11 19:57:13 +0100

    various tweaks after testing on device

--- main.ts
+++ main.ts
@@ -1,51 +1,59 @@
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.stopAllSounds()
     sound = 1 - sound
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (sound == 1) {
-        music.play(music.melodyPlayable(music.sonar), music.PlaybackMode.InBackground)
+        music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
     }
     projectile = sprites.createProjectileFromSprite(assets.image`Projectile`, Harry_Potter, 74, 0)
 })
 info.onScore(7, function () {
     effects.blizzard.endScreenEffect()
     effects.confetti.startScreenEffect()
-    kermis.playFirstPartOfKermisChorus()
-    music.rest(music.beat(BeatFraction.Half))
-    kermis.playSecondPartOfKermisChorus()
+    if (sound == 1) {
+        kermis.playFirstPartOfKermisChorus()
+        music.rest(music.beat(BeatFraction.Half))
+        kermis.playSecondPartOfKermisChorus()
+    } else {
+        pause(5000)
+    }
     effects.confetti.endScreenEffect()
     effects.blizzard.startScreenEffect()
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     if (sound == 1) {
-        music.play(music.melodyPlayable(music.beamUp), music.PlaybackMode.InBackground)
+        music.play(music.melodyPlayable(music.zapped), music.PlaybackMode.InBackground)
     }
-    info.changeScoreBy(1)
     otherSprite.destroy(effects.fire, 500)
     pause(500)
+    info.changeScoreBy(1)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     if (sound == 1) {
         music.play(music.melodyPlayable(music.smallCrash), music.PlaybackMode.InBackground)
+        controller.vibrate(100)
     }
-    info.changeLifeBy(-1)
     sprite.startEffect(effects.ashes)
     scene.cameraShake(4, 500)
     pause(500)
+    info.changeLifeBy(-1)
 })
 let Voldemort: Sprite = null
 let projectile: Sprite = null
 let Harry_Potter: Sprite = null
 let sound = 0
-sound = 0
+sound = 1
 effects.blizzard.startScreenEffect()
 scene.setBackgroundImage(assets.image`Background`)
 Harry_Potter = sprites.create(assets.image`Harry Potter`, SpriteKind.Player)
 controller.moveSprite(Harry_Potter)
 Harry_Potter.setFlag(SpriteFlag.StayInScreen, true)
 info.setLife(5)
+game.splash("READY?")
 game.onUpdateInterval(1000, function () {
     Voldemort = sprites.create(assets.image`Voldemort`, SpriteKind.Enemy)
     Voldemort.setPosition(160, randint(0, 120))
-    Voldemort.setVelocity(-1 * info.score() + -90, 0)
+    Voldemort.setVelocity(-90 + -1 * info.score(), 0)
+    Voldemort.setFlag(SpriteFlag.AutoDestroy, true)
 })
